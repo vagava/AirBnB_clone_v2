6c45acc4963894b57871f6a55e07688640467f61
vagava <vagava.123@gmail.com>
2021-08-10 17:32:53 -0500

    add single quotes  to 'hbnb_test'@'localhost'

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,10 +2,8 @@
 -- Create user hbnb_test and grant privileges on hbnb_test_db
 -- Grant privileges to hbnb_test on performance_schema
 
-CREATE DATABASE IF NOT EXISTS
-    hbnb_test_db;
-USE  hbnb_dev_db;
-CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY  'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO  hbnb_test@localhost;
-GRANT SELECT ON performance_schema.* TO  hbnb_test@localhost;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY  'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO  'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO  'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
