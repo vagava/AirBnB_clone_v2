c54e5b71ddba6a252af7e621d75d582266ab3846
vagava <vagava.123@gmail.com>
2021-08-10 17:25:33 -0500

    delete the new line

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,9 +2,7 @@
 --- Create user hbnb_dev and grant privileges on hbnb_dev_db
 --- Grant privileges to hbnb_dev on performance_schema
 
-CREATE DATABASE IF NOT EXISTS
-    hbnb_dev_db;
-
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
