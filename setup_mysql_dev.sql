952db62e21f15247dfb494e29255a0370f2e33fe
katgzco <katgzco@gmail.com>
2021-08-10 16:59:48 -0500

    Add comments and flush privileges

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,11 @@
+--- Create databases  hbnb_dev_db
+--- Create user hbnb_dev and grant privileges on hbnb_dev_db
+--- Grant privileges to hbnb_dev on performance_schema
+
 CREATE DATABASE IF NOT EXISTS
     hbnb_dev_db;
 
 CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY  'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
 GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
+FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,11 @@
+--- Create databases  hbnb_test_db
+--- Create user hbnb_test and grant privileges on hbnb_test_db
+--- Grant privileges to hbnb_test on performance_schema
+
 CREATE DATABASE IF NOT EXISTS
     hbnb_test_db;
 
 CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY  'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO  hbnb_test@localhost;
 GRANT SELECT ON performance_schema.* TO  hbnb_test@localhost;
+FLUSH PRIVILEGES;
