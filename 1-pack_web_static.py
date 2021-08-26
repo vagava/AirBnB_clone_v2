00d0b5f0e0d2a2c186092c3c641a9f309f1d8845
katgzco <katgzco@gmail.com>
2021-08-26 10:16:15 -0500

    Feature: task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+do_pack()
+"""
+
+import tarfile
+import os.path
+from datetime import datetime
+
+
+def do_pack():
+    """Fabric script that generates a .tgz archive from the contents 
+    of the web_static folder 
+    """
+    folder_to_save = 'versions'
+    time_format = datetime.now().strftime("%Y%m%d%H%M%S")
+    tar_file = 'web_static_{}.tgz'.format(time_format)
+
+    if not os.path.exists(folder_to_save):
+        os.mkdir(folder_to_save)
+
+    with tarfile.open(folder_to_save + '/' + tar_file, "w:gz") as tar:
+        tar.add('web_static', arcname=os.path.basename('web_static'))
