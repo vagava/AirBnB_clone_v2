019f627768ff50b8e8afc42a2ca63cdea851720a
katgzco <katgzco@gmail.com>
2021-08-26 11:27:16 -0500

    Fix: addvalidation of packet

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
-REQUIRED_PKG="nginx"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
-if [ "" = "$PKG_OK" ]; then
-    sudo apt-get update 
-    sudo apt-get -y install nginx
-fi
+sudo apt-get update 
+sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo tee -a /data/web_static/releases/test/index.html > /dev/null << END
