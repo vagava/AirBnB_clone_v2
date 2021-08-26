7e802920c0728c65947a07847506874796c50c8d
katgzco <katgzco@gmail.com>
2021-08-26 10:57:22 -0500

    Fix: add f flag in symbolic

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
-sudo apt-get update
+sudo apt-get update 
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
@@ -13,7 +13,7 @@ Holberton School
 </body >
 </html >
 END
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
