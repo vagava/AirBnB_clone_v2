47fadd4b7e966f5395a28325582b3e2307b38cc7
katgzco <katgzco@gmail.com>
2021-08-28 14:58:42 -0500

    Fix: change  restart for satrt

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -16,5 +16,4 @@ END
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
-sudo service nginx restart
-
+sudo service nginx start
