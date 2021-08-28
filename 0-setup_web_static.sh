0d23bac28948de3d715f7713411deb088c9138e6
katgzco <katgzco@gmail.com>
2021-08-28 15:20:04 -0500

    Test: proof checker with correct script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
-# sets up your web servers for the deployment of web_static
+# Install nginx web server
+# Step 1: Install nginx: Update apt packaging system
 if [ ! -x /usr/sbin/nginx ]; then
     sudo apt-get -y update
     sudo apt-get -y install nginx
 fi
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo tee -a /data/web_static/releases/test/index.html > /dev/null << END
-<html >
-<head >
-</head >
-<body >
-Holberton School
-</body >
-</html >
-END
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data
-sudo sed -i '/server_name _;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
-sudo service nginx start
+# Step 2: Create directories
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+# Step 3: Create fake file HTML
+echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" > /data/web_static/releases/test/index.html
+# Step 4: Create symbolic link, If the symbolic link already exists,
+# it should be deleted and recreated every time the script is ran.
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Step 5: Create permissions
+sudo chown -R ubuntu:ubuntu /data/
+# Step 6: Adding line to configurate server
+sed -i '57 i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+# Step 7: Reload/restart Nginx with nginx command:
+# To force close and restart Nginx and related processes
+sudo /etc/init.d/nginx restart
+
