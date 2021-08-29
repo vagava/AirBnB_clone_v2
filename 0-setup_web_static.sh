4feb0f9b25b03a4521c583bbb42f30ec1a7d6e48
Vanessa <vagava.123@gmail.com>
2021-08-29 16:39:00 -0500

    add sudo to all commands

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,18 +7,18 @@ then
         sudo service nginx start
 fi
 
-if ! [ -d "/data" ]; then mkdir -p /data/; fi
-if ! [ -d "/data/web_static/" ]; then mkdir -p /data/web_static/; fi
-if ! [ -d "/data/web_static/releases/" ]; then mkdir -p /data/web_static/releases/; fi
-if ! [ -d "/data/web_static/shared/" ]; then mkdir -p /data/web_static/shared/; fi
-if ! [ -d "/data/web_static/releases/test/" ]; then mkdir -p /data/web_static/releases/test/; fi
-echo " simple content, Bash script that sets up your web servers for the deployment of web_static " > /data/web_static/releases/test/index.html
+if ! [ -d "/data" ]; then sudo mkdir -p /data/; fi
+if ! [ -d "/data/web_static/" ]; then sudo mkdir -p /data/web_static/; fi
+if ! [ -d "/data/web_static/releases/" ]; then sudo mkdir -p /data/web_static/releases/; fi
+if ! [ -d "/data/web_static/shared/" ]; then sudo mkdir -p /data/web_static/shared/; fi
+if ! [ -d "/data/web_static/releases/test/" ]; then sudo mkdir -p /data/web_static/releases/test/; fi
+echo " simple content, Bash script that sets up your web servers for the deployment of web_static " > sudo /data/web_static/releases/test/index.html
 #delete /data/web_static/current
-if [ -d "data/web_static/current" ]; then rm -rf /data/web_static/current; fi
+if [ -d "data/web_static/current" ]; then sudo rm -rf /data/web_static/current; fi
 #create a symbolic link if curren no exist it create automatically
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Change permisions
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 #Configure nginx
-sed -i '37 i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '37 i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
