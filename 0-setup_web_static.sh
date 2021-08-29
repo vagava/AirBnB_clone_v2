8c740a6261968856f8d7814c7fb7ce1f81c8bf14
Vanessa <vagava.123@gmail.com>
2021-08-29 13:47:07 -0500

    add Configuration of nginx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,6 +13,12 @@ if ! [ -d "/data/web_static/releases/" ]; then mkdir -p /data/web_static/release
 if ! [ -d "/data/web_static/shared/" ]; then mkdir -p /data/web_static/shared/; fi
 if ! [ -d "/data/web_static/releases/test/" ]; then mkdir -p /data/web_static/releases/test/; fi
 echo " simple content, Bash script that sets up your web servers for the deployment of web_static " > /data/web_static/releases/test/index.html
-
+#delete /data/web_static/current
+if [ -d "data/web_static/current" ]; then rm -rf /data/web_static/current; fi
 #create a symbolic link if curren no exist it create automatically
 ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Change permisions
+chown -R ubuntu:ubuntu /data/
+#Configure nginx
+sed -i '37 i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx restart
