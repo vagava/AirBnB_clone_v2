b34eefc8033abee1fd0baefcef1ebd238932a2cc
Vanessa <vagava.123@gmail.com>
2021-08-29 17:54:53 -0500

    fix pep8

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,8 +6,9 @@ from os import path
 from fabric.api import local, put, env, run
 from datetime import datetime
 
-env.hosts = ["34.138.40.16","35.173.128.216"]
+env.hosts = ["34.138.40.16", "35.173.128.216"]
 # env.user = 'ubuntu'
+
 
 def do_pack():
     ''' Create a file .tgz base on folder web_satatic'''
@@ -29,6 +30,7 @@ def do_pack():
     print('web_static packed: {} -> {}Bytes'.format(name, size))
     return(name)
 
+
 def do_deploy(archive_path):
     '''function por unpacking files'''
     if not path.exists(archive_path):
@@ -40,16 +42,20 @@ def do_deploy(archive_path):
         # create a new folder
         run('mkdir -p /data/web_static/releases/{}/'.format(name_file))
         # uncompress file
-        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'.format(name_file, name_file))
+        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'
+            .format(name_file, name_file))
         # delete temp file
         run('rm /tmp/{}.tgz'.format(name_file))
         # move the content from web_static to new folder
-        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(name_file, name_file))
+        run('mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}/'.format(name_file,
+                                                  name_file))
         # delete folder web_static
         run('rm -rf /data/web_static/releases/{}/web_static'.format(name_file))
         # delete simbolic link
         run('rm -rf /data/web_static/current')
         # create a new simbolic link
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(name_file))
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(name_file))
     except Exception as e:
         return(False)
