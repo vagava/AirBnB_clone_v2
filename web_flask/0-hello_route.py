7e3a2c8d7417764580d7433c695b77f974d21c10
Vanessa <vagava.123@gmail.com>
2021-09-15 13:49:34 -0500

    Delete quotes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_world():
-    return 'Hello HBNB'
+    return 'Hello HBNB!'
 
-if '__name__' == '__main__':
+if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
